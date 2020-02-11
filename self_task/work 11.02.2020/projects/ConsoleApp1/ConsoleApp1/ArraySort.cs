465f5cbf95f403bc53ab72eb19d8858b81bc4cbb
Roman Sapunov <youngkicker@mail.ru>
2020-02-11 19:48:13 +0400

    changed ConsoleApp1.csproj

---
+++ self_task/work 11.02.2020/projects/ConsoleApp1/ConsoleApp1/ArraySort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ArraySort
+    {
+        public static int[] Bubble(int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return array;
+
+            while (true)
+            {
+                bool flag = false;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        flag = true;
+                    }
+                }
+                if (!flag) break;
+            }
+
+            return array;
+        }
+
+        public static int[] Select(int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return array;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int max = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[max] < array[j])
+                    {
+                        int temp = array[max];
+                        array[max] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
--- self_task/work 11.02.2020/projects/ConsoleApp1/ConsoleApp1/Program.cs
+++ self_task/work 11.02.2020/projects/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int[] array1 = { 3, 5, -34, 0, 76 };
+            array1 = ArraySort.Bubble(array1);
+            Console.Write("Bubble: ");
+            foreach (var i in array1)
+                Console.Write(i + " ");
 
+            Console.WriteLine("\n");
+
+            int[] array2 = { 3, 5, -34, 0, 76 };
+            array2 = ArraySort.Select(array2);
+            Console.Write("Select: ");
+            foreach (var i in array2)
+                Console.Write(i + " ");
+
+            Console.ReadKey();
         }
     }
 }
