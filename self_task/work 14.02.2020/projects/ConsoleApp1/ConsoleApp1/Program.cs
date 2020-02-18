b6949b48252d6a63079dd002ebb26b6c2b6b9340
Roman Sapunov <youngkicker@mail.ru>
2020-02-18 22:50:40 +0400

    исправление недочётов в isp01_sapunov_ra_discretemath/self_task/work 14.02.2020/projects/ConsoleApp1

--- self_task/work 14.02.2020/projects/ConsoleApp1/ConsoleApp1/Program.cs
+++ self_task/work 14.02.2020/projects/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,48 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _14._02._2020
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //PositiveList
-            //List<int> list = new List<int>() { 3, -9, 7, -54, 0 };
-            //list = PositiveList(list);
-            //foreach (var i in list)
-            //Console.Write(i + " ");
 
-            // InsertList
-            //List<int> list = new List<int>() { 3, -9, 7, -54, 5 };
-            //list = InsertList(list, 0);
-            //foreach (var i in list)
-            //    Console.Write(i + " ");
-
-            //ClearingList
-            //List<int> list = new List<int>() { 3, -9, 7, -54, 5 };
-            //list = ClearingList(list);
-            //foreach (var i in list)
-            //    Console.Write(i + " ");
-
-            // StringToIntList
-            //List<int> list;
-            //List<string> stringList = new List<string>() { "543", "5f43", "4513", "5a3", "-43" };
-            //list = StringToIntList(stringList);
-            //foreach (var i in list)
-            //    Console.Write(i + " ");
-
-            Console.ReadKey();
         }
 
         static List<int> PositiveList(List<int> list)
         {
             List<int> resList = new List<int>();
 
-            for (int i = 0; i < list.Count(); i++)
+            for (int i = 0; i < list.Count; i++)
                 if (list[i] > 0)
                     resList.Add(list[i]);
 
@@ -51,27 +23,21 @@ namespace _14._02._2020
 
         static List<int> InsertList(List<int> list, int x)
         {
-            for (int i = 0; i < list.Count() - 1; i++)
-            {
+            for (int i = 0; i < list.Count - 1; i++)
                 if (list[i] > 0)
-                {
-                    list[i + 1] = x;
-                    i++;
-                }
-            }
+                    list.Add(x);
 
             return list;
         }
 
         static List<int> ClearingList(List<int> list)
         {
-            for (int i = 0; i < list.Count(); i++)
-            {
+            for (int i = 0; i < list.Count; i++)
                 if (list[i] % 2 == 0)
                 {
                     list.RemoveAt(i);
+                    i--;
                 }
-            }
 
             return list;
         }
@@ -80,11 +46,9 @@ namespace _14._02._2020
         {
             List<int> resList = new List<int>();
 
-            for (int i = 0; i < list.Count(); i++)
-            {
+            for (int i = 0; i < list.Count; i++)
                 if (int.TryParse(list[i], out int x))
                     resList.Add(x);
-            }
 
             return resList;
         }
